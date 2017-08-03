b0f29613ea7a3dfa81f22cbf107e571574c096b6
KC <30301405+munifrog@users.noreply.github.com>
2017-08-02 20:33:43 -0600

    LED Clock Running Within Specified Base (Defaulting at 5)
    Original commit of an already polished project.

---
+++ BaseWhateverClock/BaseWhateverClock.ino
@@ -0,0 +1,176 @@
+// This program assumes the use of tri-color LEDs, one per digit.
+// One lead provides voltage, (which can be wired externally),
+// while the other three leads each, when set LOW allow for red, 
+// green, or blue lighting, even simultaneously. 
+// 
+// While the LED I have technically allows all three colors at the 
+// same time, it seems safer to power each color independently 
+// (i.e., during separate clock cycles). The frequency can be high
+// enough that it will not be a problem for our eyes.
+
+#include "defines.h"
+
+// Maximum unsigned long is 4,294,967,295
+unsigned long initialMillis;
+unsigned long previousMinute;
+unsigned long currentMinute;
+unsigned short digits[MAX_DIGITS];
+unsigned short currentColor;
+unsigned long elapsedMillis;
+unsigned long microsThisInterval;
+unsigned long currentSecond;
+unsigned long frequencyCutoff_R[CLOCK_BASE];
+unsigned long frequencyCutoff_G[CLOCK_BASE];
+unsigned long frequencyCutoff_B[CLOCK_BASE];
+unsigned long microsPerInterval = PRECISION_FACTOR * MICROS_PER_SECOND / FREQUENCY_IN_Hz;
+
+void setup() {
+  // put your setup code here, to run once:
+  initialMillis = millis();
+  setAllDigitalPinsOut();
+  setAllLightsOff();
+  for(int i = 0; i < CLOCK_BASE; i++) {
+    digits[CLOCK_BASE] = 0;
+  }
+  currentColor = COLOR_BLUE;
+  computeIntensityCutoffValues();
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Code within 'loop' runs repeatedly:
+
+//  microsThisInterval = (PRECISION_FACTOR * micros()) / FREQUENCY_IN_Hz;
+  microsThisInterval = PRECISION_FACTOR * micros();
+
+  // Determine seconds running; transitions at minute
+  elapsedMillis = millis() - initialMillis;
+  currentSecond = elapsedMillis / MILLIS_PER_SECOND;
+  if(currentSecond > SECONDS_PER_MINUTE){
+    currentMinute = currentSecond / SECONDS_PER_MINUTE;
+    if(currentMinute > previousMinute) {
+      previousMinute = currentMinute;
+      determineDigits(currentMinute);  
+    }
+  } else {
+    // Indicate seconds for the first minute (let's me know it's working if zero-digit equals zero-intensity)
+    determineDigits(currentSecond);  
+  }
+  
+  shineLights();
+}
+
+void setAllLightsOff() {
+  for(int i = 0; i < MAX_DIGITS; i++) {
+    digitalWrite(PIN_R[i], OFF);
+    digitalWrite(PIN_B[i], OFF);
+    digitalWrite(PIN_G[i], OFF);
+  }
+}
+
+void setAllRedOff() {
+  for(int i = 0; i < MAX_DIGITS; i++) {
+    digitalWrite(PIN_R[i], OFF);
+  }
+}
+
+void setAllBlueOff() {
+  for(int i = 0; i < MAX_DIGITS; i++) {
+    digitalWrite(PIN_B[i], OFF);
+  }
+}
+
+void setAllGreenOff() {
+  for(int i = 0; i < MAX_DIGITS; i++) {
+    digitalWrite(PIN_G[i], OFF);
+  }
+}
+
+void setAllDigitalPinsOut() {
+  for(int i = 0; i < MAX_DIGITS; i++) {
+    pinMode(PIN_R[i], OUTPUT);
+    pinMode(PIN_B[i], OUTPUT);
+    pinMode(PIN_G[i], OUTPUT);
+  }
+}
+
+void determineDigits(unsigned long minutes) {
+  unsigned long remainingMinutes = minutes;
+  for(int i = 0; i < MAX_DIGITS; i++) {
+    digits[i] = remainingMinutes % CLOCK_BASE;
+    remainingMinutes = remainingMinutes / CLOCK_BASE;
+  }
+} 
+
+void shineLights() {
+  currentColor++;
+  currentColor = currentColor % 3;
+  // Shine only one color per loop; cycle through colors
+  switch(currentColor) {
+    case COLOR_RED:
+      setAllGreenOff(); // previous was green
+      for(int i = 0; i < MAX_DIGITS; i++) {
+        digitalWrite(
+          PIN_R[i], 
+#if ON_DEMAND_COMPUTATION
+          shineThisCycle(FREQUENCY_R[digits[i]])
+#else
+          (microsThisInterval % microsPerInterval) < frequencyCutoff_R[digits[i]]
+#endif
+          ? ON : OFF
+        );
+      }
+      break;
+    case COLOR_BLUE:
+      setAllRedOff(); // previous was red
+      for(int i = 0; i < MAX_DIGITS; i++) {
+        digitalWrite(
+          PIN_B[i], 
+#if ON_DEMAND_COMPUTATION
+          shineThisCycle(FREQUENCY_B[digits[i]])
+#else
+          (microsThisInterval % microsPerInterval) < frequencyCutoff_B[digits[i]]
+#endif
+          ? ON : OFF
+        );
+      }
+      break;
+    case COLOR_GREEN:
+      setAllBlueOff(); // previous was blue
+      for(int i = 0; i < MAX_DIGITS; i++) {
+        digitalWrite(
+          PIN_G[i], 
+#if ON_DEMAND_COMPUTATION
+          shineThisCycle(FREQUENCY_G[digits[i]])
+#else
+          (microsThisInterval % microsPerInterval) < frequencyCutoff_G[digits[i]]
+#endif
+          ? ON : OFF
+        );
+      }
+      break;
+  }
+}
+
+boolean shineThisCycle(unsigned short intensity){
+  boolean bShine = false;  
+  if(intensity > 0) {
+    bShine = (microsThisInterval % microsPerInterval) < computeOnDemand(intensity);
+  }
+  return bShine;
+}
+
+void computeIntensityCutoffValues() {
+  for( int i = 0; i < CLOCK_BASE; i++ ){
+    frequencyCutoff_R[i] = computeOnDemand(FREQUENCY_R[i]);
+    frequencyCutoff_G[i] = computeOnDemand(FREQUENCY_G[i]);
+    frequencyCutoff_B[i] = computeOnDemand(FREQUENCY_B[i]);
+  }
+}
+
+unsigned long computeOnDemand(unsigned short intensity) {
+  return (intensity * microsPerInterval) / MAX_INTENSITY;
+}
+
+
---
+++ BaseWhateverClock/defines.h
@@ -0,0 +1,42 @@
+// Define the base
+const unsigned int CLOCK_BASE = 5;
+const unsigned long MAX_DIGITS = 4; // I can only fit 4 LEDs (with 3 leads each) on board
+
+const unsigned long PRECISION_FACTOR  = 1000; // one zero per point of precision; 3 --> 1000
+const unsigned long SECONDS_PER_MINUTE = 60;
+const unsigned long MILLIS_PER_SECOND = 1000;
+const unsigned long MICROS_PER_SECOND = 1000000;
+const unsigned long PICOS_PER_SECOND  = 1000000000;
+
+const int COLOR_RED = 0;
+const int COLOR_GREEN = 1;
+const int COLOR_BLUE = 2;
+
+
+// List the color frequency associated with each digit; same per LED
+const short FREQUENCY_R[CLOCK_BASE] = {  0,   0,  0,  31,  31};
+const short FREQUENCY_G[CLOCK_BASE] = {  0,  31,  0,   0,   0};
+const short FREQUENCY_B[CLOCK_BASE] = {  0,   0,  31,  0,  31};
+const short FREQUENCY_IN_Hz = 50;
+
+const int PRINT_MODULUS_AMOUNT = 100;
+const int MAX_INTENSITY = 255;
+
+// List the pins associate with different LED output leads
+const short PIN_R[MAX_DIGITS] = { 2,  5, 10, 11};
+const short PIN_B[MAX_DIGITS] = { 3,  6,  9, 12};
+const short PIN_G[MAX_DIGITS] = { 4,  7,  8, 13};
+
+#define ON_DEMAND_COMPUTATION 0
+//#define ON_DEMAND_COMPUTATION 1
+
+// Define what means on/off so this can be changed quickly
+#define USING_PRO_MINI 1
+#if USING_PRO_MINI
+  #define ON LOW
+  #define OFF HIGH
+#else
+  #define ON HIGH
+  #define OFF LOW
+#endif
+
